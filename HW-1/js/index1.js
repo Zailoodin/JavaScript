582a40f554db740cbbcfac71ade8b8b025b3da12
Zailoodin Tilebaldyev <88503274+Zailoodin@users.noreply.github.com>
2022-01-29 01:43:50 +0600

    Added Task-1
    Текшерип баа берип койунуздар!

---
+++ HW-1/js/index1.js
@@ -0,0 +1,6 @@
+// 1.Простое сообщение: сохраните текстовое сообщение в переменной и выведите его в браузере
+
+let myName = "#1. Hello, I'm Zailoodin. <br>";
+let myAge = 20;
+
+console.log(myName, myAge,'<br>');
---
+++ HW-1/js/index3.js
@@ -0,0 +1,7 @@
+// 3.Напишите несколько строк кода, выводящих на экран ваше имя и почтовый адрес.
+
+const name = 'Zailoodin <br>';
+const email = 'zailoodin.tilebaldyev.kk@gmail.com';
+
+document.write(`My name is - ${name}`);
+document.write(`My email is - "${email}" <br> <hr>`);
